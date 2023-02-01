bd5c06dabc0a86948310875b91c4e34bcedbb7f8
Praveen Suthar <praveensuthar7230@gmail.com>
2023-02-01 06:24:08 +0000

    updated

--- models/example/test_file.sql
+++ models/example/test_file.sql
@@ -1,3 +1,13 @@
-SELECT  
-  *
-FROM "names"
+select
+"height",
+listagg("known_for_movies", ',') as "temp"
+from "names" 
+group by "height"
+
+select
+"name",
+listagg("known_for_movies", ',') as "known_for_movies"
+from "names" 
+group by "name"
+
+select * from "names"
