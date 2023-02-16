6fbcb43c981d73171de3e7c4999a61902c543c03
praveensuthar7230@gmail.com <praveensuthar7230@gmail.com>
2023-02-16 10:38:54 +0000

    change

--- models/flatten_demo/sample_flatten.sql
+++ models/flatten_demo/sample_flatten.sql
@@ -7,29 +7,29 @@
 -- 	json_raw ,
 -- 	lateral flatten( input => data );
 
-select * 
-from json_raw jr,
-lateral flatten(input => jr.data);
+-- select * 
+-- from json_raw jr,
+-- lateral flatten(input => jr.data);
 
-select f.seq, f.key, f.path, f.value
-from json_raw jr,
-lateral flatten(input => jr.data:products, recursive => true) f;
+-- select f.seq, f.key, f.path, f.value
+-- from json_raw jr,
+-- lateral flatten(input => jr.data:products, recursive => true) f;
 
 
-with low_level_flatten as (
-	select f.key as json_key, f.path as json_path, 
-	f.value as json_value
-	from json_raw, 
-	lateral flatten(input => data:products, recursive => true ) f
-)
+-- with low_level_flatten as (
+-- 	select f.key as json_key, f.path as json_path, 
+-- 	f.value as json_value
+-- 	from json_raw, 
+-- 	lateral flatten(input => data:products, recursive => true ) f
+-- )
 
-select distinct json_path
-from low_level_flatten
-where not contains(json_value, '{')
+-- select distinct json_path
+-- from low_level_flatten
+-- where not contains(json_value, '{')
 
 {{
   generate_flatten_json(
     model_name = ref('json_data_model'),
-    json_column = 'data:products'
+    json_column = 'data'
   )
 }}
